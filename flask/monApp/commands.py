003aeb569c6c33b88e3d05088d06816e4cc64593
Tifany Meunier <tifany.meunier@etu.univ-orleans.fr>
2025-12-03 10:45:33 +0100

    ajout données

--- flask/monApp/commands.py
+++ flask/monApp/commands.py
@@ -49,72 +49,111 @@ def seed_db():
         pf2 = Plateforme(nomPlateforme="RoboTest", nbPersonnes=3, cout=2000, intervalleMaintenance=30, derniereMaintenance=None, prochaineMaintenance=None)
         pf2.laboratoire = lab2
 
-        db.session.add_all([pf1, pf2])
+        pf3 = Plateforme(nomPlateforme="ThermoScan", nbPersonnes=2, cout=1000, intervalleMaintenance=180, derniereMaintenance="2025-05-15", prochaineMaintenance="2025-11-11")
+        pf3.laboratoire = lab1 # Attaché au LIRMM
+        
+        db.session.add_all([pf1, pf2, pf3])
         db.session.commit() 
 
-        # --- 3. Équipements ---
+        # --- Équipements ---
         eq1 = Equipement(nomEquipement="Microscope Électronique")
         eq2 = Equipement(nomEquipement="Sonde de Température")
         eq3 = Equipement(nomEquipement="Bras Robotique KUKA")
-        db.session.add_all([eq1, eq2, eq3])
-        db.session.commit() 
+        eq4 = Equipement(nomEquipement="Spectromètre Raman")
+        eq5 = Equipement(nomEquipement="Chambre Climatique")
+        eq6 = Equipement(nomEquipement="Imprimante 3D Métal")
+        
+        db.session.add_all([eq1, eq2, eq3, eq4, eq5, eq6])
+        db.session.commit() # Obtient les IDs des équipements (idE)
 
-        # --- 4. Contenir (Plateforme -> Équipement) ---
+        # --- Contenir (Plateforme -> Équipement) ---
         c1 = Contenir(nomPlateforme=pf1.nomPlateforme, idE=eq1.idE)
         c2 = Contenir(nomPlateforme=pf1.nomPlateforme, idE=eq2.idE)
         c3 = Contenir(nomPlateforme=pf2.nomPlateforme, idE=eq3.idE)
-        db.session.add_all([c1, c2, c3])
+        c4 = Contenir(nomPlateforme=pf3.nomPlateforme, idE=eq4.idE) # ThermoScan -> Spectromètre
+        c5 = Contenir(nomPlateforme=pf3.nomPlateforme, idE=eq5.idE) # ThermoScan -> Chambre Climatique
+        c6 = Contenir(nomPlateforme=pf1.nomPlateforme, idE=eq6.idE) # NanoFab -> Imprimante 3D
+        c7 = Contenir(nomPlateforme=pf2.nomPlateforme, idE=eq5.idE) # RoboTest -> Chambre Climatique (partagé)
+        
+        db.session.add_all([c1, c2, c3, c4, c5, c6, c7])
 
-        # --- 5. Habilitations ---
+        # --- Habilitations ---
         h1 = Habilitation(type="Électrique")
         h2 = Habilitation(type="Chimique")
         h3 = Habilitation(type="Radiations")
         h4 = Habilitation(type="Biologique")
         db.session.add_all([h1, h2, h3,h4])
 
-        # --- 6. Nécessite (Plateforme -> Habilitation) ---
-        n1 = Necessite(type=h1.type, nomPlateforme=pf1.nomPlateforme)
-        n2 = Necessite(type=h3.type, nomPlateforme=pf1.nomPlateforme)
-        n3 = Necessite(type=h4.type, nomPlateforme=pf2.nomPlateforme)
-        db.session.add(n1)
-        db.session.add(n2)
-        db.session.add(n3)
+        # --- Nécessite (Plateforme -> Habilitation) ---
+        n1 = Necessite(type=h1.type, nomPlateforme=pf1.nomPlateforme) # NanoFab -> Électrique
+        n2 = Necessite(type=h3.type, nomPlateforme=pf1.nomPlateforme) # NanoFab -> Radiations
+        n3 = Necessite(type=h4.type, nomPlateforme=pf2.nomPlateforme) # RoboTest -> Biologique
+        n4 = Necessite(type=h2.type, nomPlateforme=pf3.nomPlateforme) # ThermoScan -> Chimique
+        n6 = Necessite(type=h2.type, nomPlateforme=pf1.nomPlateforme) # NanoFab -> Chimique
         
-        # --- 7. Campagnes ---
+        db.session.add_all([n1, n2, n3, n4, n6])
+        
+        # --- Campagnes ---
         camp1 = Campagne(date="2025-12-01", duree=7,nomPlateforme = pf1.nomPlateforme,lieu="Orléans")
         camp2 = Campagne(date="2026-01-15", duree=3,nomPlateforme = pf2.nomPlateforme,lieu="Forêt")
+        camp3 = Campagne(date="2026-03-20", duree=10, nomPlateforme=pf1.nomPlateforme, lieu="Grenoble")
+        camp4 = Campagne(date="2025-11-25", duree=2, nomPlateforme=pf3.nomPlateforme, lieu="Sous-Sol")
+        camp5 = Campagne(date="2026-04-05", duree=5, nomPlateforme=pf2.nomPlateforme, lieu="Toulouse")
         
-        db.session.add_all([camp1, camp2])
-        db.session.commit() 
+        db.session.add_all([camp1, camp2, camp3, camp4, camp5])
+        db.session.commit() # Obtient les IDs des campagnes (numCampagne)
 
-        # --- 9. Posséder (Personne -> Habilitation) ---
-        pos1 = Posseder(type=h1.type, idP=1)
-        pos2 = Posseder(type=h2.type, idP=2)
-        pos3 = Posseder(type=h2.type, idP=1)
-        db.session.add_all([pos1, pos2,pos3])
+        # --- Posséder (Personne -> Habilitation) ---
+        pos1 = Posseder(type=h1.type, idP=1) # idP 1: Électrique
+        pos2 = Posseder(type=h2.type, idP=2) # idP 2: Chimique
+        pos3 = Posseder(type=h2.type, idP=1) # idP 1: Chimique
+        pos4 = Posseder(type=h3.type, idP=3) # idP 3: Radiations
+        pos5 = Posseder(type=h4.type, idP=1) # idP 1: Biologique
+        pos7 = Posseder(type=h1.type, idP=3) # idP 3: Électrique
+        
+        db.session.add_all([pos1, pos2, pos3, pos4, pos5, pos7])
 
-        # --- 10. Participer (Campagne -> Personne) ---
-        part1 = Participer(numCampagne=camp1.numCampagne, idP=1)
-        part2 = Participer(numCampagne=camp1.numCampagne, idP=2)
-        part3 = Participer(numCampagne=camp2.numCampagne, idP=3)
-        db.session.add_all([part1, part2, part3])
+        # --- Participer (Campagne -> Personne) ---
+        part1 = Participer(numCampagne=camp1.numCampagne, idP=1) # Campagne 1 -> idP 1
+        part2 = Participer(numCampagne=camp1.numCampagne, idP=2) # Campagne 1 -> idP 2
+        part3 = Participer(numCampagne=camp2.numCampagne, idP=3) # Campagne 2 -> idP 3
+        part4 = Participer(numCampagne=camp3.numCampagne, idP=1) # Campagne 3 -> idP 1
+        part6 = Participer(numCampagne=camp4.numCampagne, idP=2) # Campagne 4 -> idP 2
+        part9 = Participer(numCampagne=camp5.numCampagne, idP=3) # Campagne 5 -> idP 3
+        db.session.add_all([part1, part2, part3, part4,  part6, part9])
 
-        # --- 11. Fichiers ---
+        # --- Fichiers ---
         f1 = Fichier(nomFichier="Résultats_NanoFab_1201.pdf")
-        f2 = Fichier(nomFichier="aaaaaaaaaaaaaa.pdf")
-        db.session.add(f1,f2)
+        f2 = Fichier(nomFichier="Analyse_Acier_C40_Ref.pdf")
+        f3 = Fichier(nomFichier="Rapport_ThermoScan_1125.txt")
+        f4 = Fichier(nomFichier="Photos_Campagne3_Grenoble.zip")
+        f5 = Fichier(nomFichier="Note_Prepa_Campagne5.docx")
+
+        db.session.add_all([f1,f2,f3,f4,f5])
         db.session.commit()
 
-        # --- 12. Échantillons ---
+        # --- Échantillons ---
         ech1 = Echantillon(typeE="Polymère", nomSpecifique="PPC-1", commentaire="Test initial", numCampagne=camp1.numCampagne, idP=1)
         ech2 = Echantillon(typeE="Métal", nomSpecifique="Acier C40", commentaire="Échantillon de référence", numCampagne=camp2.numCampagne, idP=3)
-        ech3 = Echantillon(typeE="Métal", nomSpecifique="Acier C40", commentaire="Échantillon de référence", numCampagne=camp1.numCampagne, idP=1)
-        db.session.add_all([ech1, ech2,ech3]) 
-        ech1.fichier = f1 
-        ech3.fichier = f2 
+        ech3 = Echantillon(typeE="Polymère", nomSpecifique="PPC-1", commentaire="Reprise du test initial", numCampagne=camp1.numCampagne, idP=1)
+        ech4 = Echantillon(typeE="Verre", nomSpecifique="Verre QZ-1", commentaire="Échantillon pour Spectromètre", numCampagne=camp3.numCampagne, idP=1)
+        ech5 = Echantillon(typeE="Bois", nomSpecifique="Chêne traité", commentaire="Test de vieillissement", numCampagne=camp3.numCampagne, idP=3)
+        ech6 = Echantillon(typeE="Polymère", nomSpecifique="PVC souple", commentaire="Test de température extrême", numCampagne=camp4.numCampagne, idP=2)
+        ech7 = Echantillon(typeE="Métal", nomSpecifique="Alliage Titane", commentaire="Test de résistance RoboTest", numCampagne=camp5.numCampagne, idP=2)
+        ech8 = Echantillon(typeE="Liquide", nomSpecifique="Éthanol Pur", commentaire="Contrôle qualité", numCampagne=camp1.numCampagne, idP=3)
+        ech9 = Echantillon(typeE="Bois", nomSpecifique="Chêne non traité", commentaire="Référence pour test de vieillissement", numCampagne=camp3.numCampagne, idP=3)
 
+
+        db.session.add_all([ech1, ech2, ech3, ech4, ech5, ech6, ech7, ech8, ech9]) 
+        
+        ech1.fichier = f1 
+        ech2.fichier = f2
+        ech6.fichier = f3 
+        ech5.fichier = f4
+        # ech7 n'a pas de fichier associé
+        
         db.session.commit()
-        print("✅ Base de données peuplée avec des données de test !")
+        print("✅ Base de données peuplée avec des données de test additionnelles !")
 
     except Exception as e:
         db.session.rollback()
